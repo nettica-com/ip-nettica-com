9e06250215fc6dc53dd5a47b9c9162275ff7c00e
Alan Graham <alan@nettica.com>
2024-06-10 11:22:33 -0700

    add support for json, xml, html

--- _worker.js
+++ _worker.js
@@ -12,22 +12,42 @@ export default {
 	async fetch(request, env, ctx) {
 	  // Get the IP address from the CF-Connecting-IP header
 	    const ip = request.headers.get('CF-Connecting-IP');
+        var   ah = request.headers.get('Accept');
         const url = new URL(request.url);
         const path = url.pathname;
 
-        console.log(`ip=${ip}\t path=${path}\t`)
+        console.log(`ip=${ip}\t path=${path}\t Accept=${ah}`)
 
         // if the path is not "/" return static asset
         if (path !== "/") {
             return env.ASSETS.fetch(request);
         }
-        
-	    console.log(`ip=${ip}`)  
-	    // Return the IP address in the response
-	    return new Response(ip, {
-		    headers: {
-		        'Content-Type': 'text/plain'
-		    }
-	  });
+
+        if (ah && ah.includes("text/html")) {
+            ah = "text/html";
+        }
+
+        switch (ah) {
+
+            case "*/*":
+            case null:
+                return new Response( ip, { headers: { 'Content-Type': 'text/plain' } });
+
+            case "application/json":
+            case "text/json":
+                return new Response(`{"ip":"${ip}"}`, { headers: { 'Content-Type': 'application/json' } });
+
+            case "application/xml":
+            case "text/xml":
+                return new Response(`<ip>${ip}</ip>`, { headers: { 'Content-Type': 'application/xml' } });
+
+            case "text/html":
+                return new Response(`<html><body><p>${ip}</p><a href="https://nettica.com/">sponsored by nettica.com</a></body></html>`, { headers: { 'Content-Type': 'text/html' } });
+            
+            default:
+                return new Response( ip, { headers: { 'Content-Type': 'text/plain' } });
+        }
+
+        return new Response("Invalid request", { status: 400 });
    },
 };
